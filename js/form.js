881e6ad7c021a3ca8b6a5f22d7e9d3bd3aef775f
Sakamoto0608 <kbc20f02@stu.kawahara.ac.jp>
2021-07-13 11:39:04 +0900

    update:日付入力修正の追加
    4-31や2-31を指定した場合に適切な最大日に設定しなおす機能を追加。
    それにともないうるう年チェックも追加。

--- js/form.js
+++ js/form.js
@@ -14,4 +14,41 @@ $(function(){
     for(var i = 1; i <= 31;i++){
         $(".day").append("<option>" + i + "</option>");
     }
+    //日付調整
+    $('.birthday').change(function(){
+        var month = $('.birthday.month').val();
+        month = Number(month);
+        var day = $('.birthday.day').val();
+        day = Number(day);
+        switch(month){
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                if(day >= 31){
+                    $('.birthday.day').val(30);
+                    
+                }
+                break;
+            case 2:
+                if(day >= 29){
+                    var year = $('.birthday.year').val();
+                    year = Number(year);
+                    if(isLeapYear(year)){
+                        $('.birthday.day').val(29);
+                        return;
+                    }
+                    $('.birthday.day').val(28);
+                }
+        }
+        // var html = '<span style="color:red">セレクトボックス</span>でchangeイベントが実行されました！';
+        // $('#test').html(html);
+    });
+    //うるう年チェック
+    function isLeapYear(year){
+        if( (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+          return true;
+        }
+        return false;
+    }
 });
